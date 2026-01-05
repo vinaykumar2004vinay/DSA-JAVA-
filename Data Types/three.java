5509727a56585e3cd8067c47d346440cf1358a91
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-01-05 11:24:01 +0530

    JAVA

---
+++ Data Types/three.java
@@ -0,0 +1,31 @@
+class three
+{
+    public static void main(String[] args)
+    {
+        int i = 5215452;
+        byte b = 124;
+        short s = 250;
+        long l = 4156451l;
+
+        float f = 452.74f;
+        double d = 4518697.45658;
+
+        char c = 'v';
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println("-------------------------");
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println("-------------------------");
+        System.out.println(c);
+        System.out.println("-------------------------");
+        System.out.println(b1);
+        System.out.println(b2);
+    }
+}
---
+++ Data Types/two.java
@@ -0,0 +1,10 @@
+class two
+{
+    public static void main(String[] args)
+    {
+        double num1 = 5.5d;
+        double num2 = 6.7d;
+        double result = num1 + num2;
+        System.out.println(result);
+    }
+}
